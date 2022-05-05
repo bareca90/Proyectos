f5864b914194871a7f2155e7203c5f4c06701dd0
Bairon Reyes <37020508+bareca90@users.noreply.github.com>
2022-05-05 14:47:52 -0500

    Update index.php

--- Php/DasCamara/index.php
+++ Php/DasCamara/index.php
@@ -1,7 +1,7 @@
 <?php
 
-    $serverName = "10.100.120.7";
-    $connectionInfo = array( "Database"=>"SipeDes", "UID"=>"sa", "PWD"=>"84+-blaster32","CharacterSet"=>"UTF-8");
+    $serverName = "Server";
+    $connectionInfo = array( "Database"=>"SipeDes", "UID"=>"user", "PWD"=>"passw","CharacterSet"=>"UTF-8");
     $con = sqlsrv_connect($serverName, $connectionInfo);
     if(!$con) {     
         echo"Error al conectar a la base de datos"; 
