2a4ea29f739294fb13b7fde664b9d49494489e16
Bairon Reyes <bareca90@gmail.com>
2022-06-17 14:03:43 -0500

    PrimerCommit
    Se creo el api para devolver los datos del cliente

---
+++ Nodejs/ApiWebDatos/configuraciones/configuracion_base.js
@@ -0,0 +1,13 @@
+var config = {
+    user: 'user',
+    password: 'xxxxxxxxx',
+    server: 'nombre del server o ip',
+    database: 'sipeDes',
+    driver: 'msnodesqlv8',
+    options: {
+        enableArithAbort: true,
+        trustedConnection: true
+    }
+
+};
+module.exports = config;
---
+++ Nodejs/ApiWebDatos/rutas/consultar_sp.js
@@ -0,0 +1,33 @@
+const config = require('../configuraciones/configuracion_base');
+/* const sql = require('mssql'); */
+const express = require('express');
+const sql = require('mssql/msnodesqlv8');
+const app = express();
+
+/*Conexion a Sp que devuelve los datos de los clientes*/
+let datosCliente = async(query, cliente) => {
+    try {
+        let pool = await sql.connect(config);
+        let clientes = await pool.request()
+            .input('cliente', sql.Char, cliente)
+            .execute(query);
+        /* console.log(clientes); */
+        return clientes.recordsets;
+        /* return clientes; */
+    } catch (error) {
+        console.log(error);
+    }
+
+
+}
+
+/*Ruta para Obtener Datos Cliente */
+app.get('/datoscliente', function(req, respuesta) {
+    let cliente = req.query.cliente ;
+    let query = 'Sp_Datos_Cliente';
+    datosCliente(query, cliente).then(datoscliente => {
+        respuesta.json(datoscliente[0]);
+    })
+
+})
+module.exports = app;
